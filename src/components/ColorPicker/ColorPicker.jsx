d70dcaac98d6c74592d75e5af37690cd4c7d9e4b
Maryna-Korbet <m22v86k@gmail.com>
2022-12-20 20:12:43 +0200

    class ColorPicker

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -1,19 +1,24 @@
 import css from 'components/ColorPicker/ColorPicker.module.css';
+import { Component } from 'react';
 
-const ColorPicker = ({ options }) => (
-    <div className ={css.container}>
+class ColorPicker extends Component {
+    render() {
+        return (
+            <div className ={css.container}>
         <h2 className ={css.title}>
             <div>
-                {options.map(({ label, color }) => (
-                    <span>
+                {this.props.options.map(({ label, color }) => (
+                    <button type ="button">
                         key={label}
                         className ={css.label}
                         style ={{backgroundColor: color}}
-    </span>
+    </button>
 ))}
             </div>
         </h2>
     </div>
-)
+        )
+    }
+}
 
 export default ColorPicker;
